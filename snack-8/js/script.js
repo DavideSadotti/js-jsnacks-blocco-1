a8cb39be4426425f81981096a890d9cbdd4509f1
Davide <d.sadotti@gmail.com>
2021-03-03 12:51:49 +0100

    Snack 8 ho implementato un controllo con do-while

--- snack-8/js/script.js
+++ snack-8/js/script.js
@@ -2,10 +2,14 @@ var numero;
 var cifra;
 var somma = 0;
 
-numero = prompt("Inserisci un numero a 4 cifre!");
-if(numero.length > 4 || numero.length < 4){
-  numero = prompt("RIPROVACI! Inserisci un numero a 4 cifre!");
-}
+do{
+  do{
+    numero = prompt("Inserisci un numero a 4 cifre!");
+  }while(isNaN(numero))
+}while(numero.length > 4 || numero.length < 4);
+// if(numero.length > 4 || numero.length < 4){
+//   numero = prompt("RIPROVACI! Inserisci un numero a 4 cifre!");
+// }
 
 for(var i = 0; i < 4; i++){
   cifra = parseInt(numero.charAt(i));
